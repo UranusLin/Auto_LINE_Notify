17c9841b15836e5848477dce6a34058e25d05a92
Morris.Lin <Morris.Lin@nogle.com>
2022-07-22 00:31:55 +0800

    feat: add auto resize to 3MB function feat: change sent image message will be space

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import csv
 import shutil
 from copy import deepcopy
 from datetime import datetime, timezone, timedelta
+import os
+from PIL import Image
 
 import requests
 import glob
@@ -28,6 +30,7 @@ def sent_to_line_notify(content: dict, token: dict):
     headers = {"Authorization": "Bearer " + token.get("token")}
     content = deepcopy(content)
     if content.get("type") == "image":
+        resize_image(content.get("files").get("imageFile"))
         content["files"] = {
             "imageFile": open(content.get("files").get("imageFile"), "rb")
         }
@@ -60,7 +63,7 @@ def parser_file(files: list) -> list:
             sent_content_list.append({
                 "type": "image",
                 "data": {
-                    "message": file_name
+                    "message": " ",
                 },
                 "files": {
                     "imageFile": file
@@ -99,6 +102,73 @@ def white_csv_log(log: list):
     with open(path, "a+") as f:
         csv_write = csv.writer(f)
         csv_write.writerow(log)
+
+
+def resize_image(path):
+    orig_size = get_size_format(os.path.getsize(path))
+    if float(orig_size[0:-2]) > 3:
+        compress_img(path, 3 / float(orig_size[0:-2]))
+
+
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
+
+
+def compress_img(image_name, new_size_ratio=0.9, quality=90, width=None, height=None, to_jpg=True):
+    # load the image to memory
+    img = Image.open(image_name)
+    # print the original image shape
+    print("[*] Image shape:", img.size)
+    # get the original image size in bytes
+    image_size = os.path.getsize(image_name)
+    # print the size before compression/resizing
+    print("[*] Size before compression:", get_size_format(image_size))
+    if new_size_ratio < 1.0:
+        # if resizing ratio is below 1.0, then multiply width & height with this ratio to reduce image size
+        img = img.resize((int(img.size[0] * new_size_ratio), int(img.size[1] * new_size_ratio)), Image.ANTIALIAS)
+        # print new image shape
+        print("[+] New Image shape:", img.size)
+    elif width and height:
+        # if width and height are set, resize with them instead
+        img = img.resize((width, height), Image.ANTIALIAS)
+        # print new image shape
+        print("[+] New Image shape:", img.size)
+    # split the filename and extension
+    filename, ext = os.path.splitext(image_name)
+    # make new filename appending _compressed to the original file name
+    if to_jpg:
+        # change the extension to JPEG
+        new_filename = f"{filename}{ext}"
+    else:
+        # retain the same extension of the original image
+        new_filename = f"{filename}{ext}"
+    try:
+        # save the image with the corresponding quality and optimize set to True
+        img.save(new_filename, quality=quality, optimize=True)
+    except OSError:
+        # convert the image to RGB mode first
+        img = img.convert("RGB")
+        # save the image with the corresponding quality and optimize set to True
+        img.save(new_filename, quality=quality, optimize=True)
+    print("[+] New file saved:", new_filename)
+    # get the new image size in bytes
+    new_image_size = os.path.getsize(new_filename)
+    # print the new size in a good format
+    print("[+] Size after compression:", get_size_format(new_image_size))
+    # calculate the saving bytes
+    saving_diff = new_image_size - image_size
+    # print the saving percentage
+    print(f"[+] Image size change: {saving_diff / image_size * 100:.2f}% of the original image size.")
 
 
 if __name__ == "__main__":
